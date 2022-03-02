8dbb18fb2efacf1079705e4afad8ec85c18812dc
y70131 <yuukiyamada2005@gmail.com>
2022-03-02 20:14:30 +0900

    クラスが問題についての情報を持つように変更

--- game/questioner.js
+++ game/questioner.js
@@ -1,6 +1,15 @@
 import { Field } from "./field";
 
 export class Questioner {
+    constructor(){
+        // 何世代までに問題の盤面にしなければならないか
+        this.generationLimit = 20;
+        // 問題の盤面
+        this.ploblem = {};
+        // 問題の解答例
+        this.correctField = {};
+    }
+
     // 問題を生成する
     generateProblem() {
         return new Field();
