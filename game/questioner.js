bd16201ced29f4e80f23026346a981275605afd9
y70131 <yuukiyamada2005@gmail.com>
2022-03-07 22:56:58 +0900

    judgeAnswerを引数をコピーしてから処理を行うように変更
    参照が渡り、呼び出し元に変更が加わっていたことの修正をした

--- game/questioner.js
+++ game/questioner.js
@@ -1,4 +1,4 @@
-import { Field } from "./field.js";
+import { Field, copyField } from "./field.js";
 import { Location } from "./location.js"
 import { WIDTH, HEIGHT } from "../appConfig.js"
 
@@ -87,6 +87,8 @@ export class Questioner {
     // 解答の盤面を受け取り、正解ならtrueと問題と盤面が一致する世代を、
     // 不正解ならfalseとgenerationLimitまたはgenerationLimit以下の盤面のライフが無くなる世代を返す
     judgeAnswer(answerField) {
+        let answer = copyField(answerField);
+        answer.onChange = function (location, cell) { };
         let generation = 1;
         let judge = false;
         let gene = this.generationUpperLimit;
@@ -96,28 +98,28 @@ export class Questioner {
         while (generation <= this.generationUpperLimit) {
             count = 0;
 
-            for (let h = 0; h < answerField.height; h++) {
-                for (let w = 0; w < answerField.width; w++) {
-                    if (answerField.field[h][w].isAlive == this.problem.field[h][w].isAlive) {
+            for (let h = 0; h < answer.height; h++) {
+                for (let w = 0; w < answer.width; w++) {
+                    if (answer.field[h][w].isAlive == this.problem.field[h][w].isAlive) {
                         count++;
                     }
 
                 }
             }
 
-            if ((count == answerField.width * answerField.height) && (generation >= this.generationLowerLimit)) {
+            if ((count == answer.width * answer.height) && (generation >= this.generationLowerLimit)) {
 
                 judge = true;
                 gene = generation;
                 break;
             }
 
-            if (answerField.countLives() == 0) {
+            if (answer.countLives() == 0) {
                 gene = generation;
                 break;
             }
 
-            answerField.updateLivesStatus();
+            answer.updateLivesStatus();
             generation++;
 
 
